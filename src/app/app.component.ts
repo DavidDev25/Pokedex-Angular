3cb5be8048e6b712f16bf830fa83c4f66ad426ba
DavidDev25 <david.werner@david-werner.dev>
2025-04-25 18:50:32 +0200

    Add LanguageSwitcherComponent to app component for language selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LanguageSwitcherComponent } from "./components/language-switcher/language-switcher.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LanguageSwitcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
