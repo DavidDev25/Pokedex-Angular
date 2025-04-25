13684b82b0f8141cae1341395320812fadaff70b
DavidDev25 <david.werner@david-werner.dev>
2025-04-25 18:50:45 +0200

    Refactor PokemonService to include language parameter and enhance species data retrieval

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,17 +1,46 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map, switchMap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PokemonService {
   private http = inject(HttpClient);
-  private apiUrl = 'https://pokeapi.co/api/v2/pokemon';
+  private apiUrl = 'https://pokeapi.co/api/v2';
 
-  getPokemonList(offset = 0, limit = 20): Observable<any> {
-    return this.http.get(`${this.apiUrl}?offset=${offset}&limit=${limit}`);
+  getPokemonList(offset = 0, limit = 20, language = 'en'): Observable<any> {
+    return this.http.get(`${this.apiUrl}/pokemon?offset=${offset}&limit=${limit}`)
+      .pipe(
+        switchMap((data: any) => {
+          const pokemonList = data.results;
+          const speciesRequests = pokemonList.map((pokemon: any) =>
+            this.getPokemonSpecies(pokemon.name)
+          );
+          return Promise.all(speciesRequests).then(speciesData => {
+            return {
+              ...data,
+              results: pokemonList.map((pokemon: any, index: number) => ({
+                ...pokemon,
+                speciesInfo: speciesData[index]
+              }))
+            };
+          });
+        })
+      );
   }
 
-  getPokemonByName(name: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/${name}`);
+  getPokemonByName(name: string, language = 'en'): Observable<any> {
+    return this.http.get(`${this.apiUrl}/pokemon/${name}`).pipe(
+      switchMap(pokemonData => 
+        this.getPokemonSpecies(name).then(speciesData => ({
+          ...pokemonData,
+          speciesInfo: speciesData
+        }))
+      )
+    );
+  }
+
+  private async getPokemonSpecies(name: string): Promise<any> {
+    const speciesData = await this.http.get(`${this.apiUrl}/pokemon-species/${name}`).toPromise();
+    return speciesData;
   }
 }
