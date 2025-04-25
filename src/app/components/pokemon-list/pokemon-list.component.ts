84a9b62124167b9de4df5cc845c9800b45bde3d4
DavidDev25 <david.werner@david-werner.dev>
2025-04-25 14:27:18 +0200

    Implement Pokémon list component with data fetching and display logic

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-list',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule, RouterLink], 
   templateUrl: './pokemon-list.component.html',
-  styleUrl: './pokemon-list.component.sass'
+  styleUrls: ['./pokemon-list.component.sass'] 
 })
-export class PokemonListComponent {
+export class PokemonListComponent implements OnInit {
+  pokemons: any[] = [];
+  isLoading = true;
+  error: string | null = null;
+  private pokemonService = inject(PokemonService);
 
+  ngOnInit(): void {
+    this.pokemonService.getPokemonList(0, 20).subscribe({
+      next: (data) => {
+        this.pokemons = data.results;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = 'Fehler beim Laden der Pokémon-Liste';
+        this.isLoading = false;
+      }
+    });
+  }
 }
