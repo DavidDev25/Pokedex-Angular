9b07f70c6f399fa55fd088e763c0ff8e43a285d7
DavidDev25 <david.werner@david-werner.dev>
2025-04-25 14:32:18 +0200

    Refactor PokémonDetailComponent to use standalone component structure and inline template

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../../services/pokemon.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-detail',
-  imports: [],
-  templateUrl: './pokemon-detail.component.html',
-  styleUrl: './pokemon-detail.component.sass'
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div *ngIf="pokemon">
+      <h2>{{ pokemon.name | titlecase }}</h2>
+      <img [src]="pokemon.sprites.front_default" alt="{{ pokemon.name }}">
+      <ul>
+        <li *ngFor="let type of pokemon.types">
+          {{ type.type.name }}
+        </li>
+      </ul>
+      <h3>Abilities</h3>
+    </div>
+  `
 })
-export class PokemonDetailComponent {
+export class PokemonDetailComponent implements OnInit {
+  pokemon: any;
+  private route = inject(ActivatedRoute);
+  private pokemonService = inject(PokemonService);
 
+  ngOnInit() {
+    const name = this.route.snapshot.paramMap.get('name');
+    if (name) {
+      this.pokemonService.getPokemonByName(name).subscribe(data => {
+        this.pokemon = data;
+      });
+    }
+  }
 }
