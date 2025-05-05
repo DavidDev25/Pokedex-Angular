e39548d494ec01975c99ae13ea7979490c44c542
DavidDev25 <david.werner@david-werner.dev>
2025-05-05 17:25:57 +0200

    Add SearchService to manage search term state with BehaviorSubject

---
+++ src/app/services/search.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private searchTerms = new BehaviorSubject<string>('');
+  
+  currentSearchTerm$ = this.searchTerms.asObservable();
+  
+  updateSearchTerm(term: string): void {
+    this.searchTerms.next(term);
+  }
+}
